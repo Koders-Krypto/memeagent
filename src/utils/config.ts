af162020bd4c775f0c3e68e185da215b162e1e6f
AnkithVijay <vijay2ankith@gmail.com>
2024-12-08 12:41:24 +0530

    Update config.ts

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,7 +1,7 @@
 import { CHAIN_NAMESPACES } from "@web3auth/base";
 
 export const getChainConfig = () => {
-    return bnbSepolia
+    return baseSepolia
     //bnbSepolia, polygonMumbai, Supratestnet baseSepolia; 
 }
 
